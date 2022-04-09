8bfdc5d5ea66028e11ac0c8049b84d5748285e47
moisalucian <moisamihailucian@gmail.com>
2022-04-09 15:05:42 +0300

    third commit

--- src/main/java/com/sda/lucianmoisa/bookstore/config/ApplicationConfig.java
+++ src/main/java/com/sda/lucianmoisa/bookstore/config/ApplicationConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan("com.sda.crisangabi.bookstore")
+@ComponentScan("com.sda.lucianmoisa.bookstore")
 public class ApplicationConfig {
 
 }
